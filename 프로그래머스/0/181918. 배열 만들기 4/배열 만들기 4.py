727e07aea46398cc34cf192832cc48da5c78451c
Sanga <103806889+tkddk0108@users.noreply.github.com>
2024-09-09 05:20:35 +0900

    [level 0] Title: 배열 만들기 4, Time: 59.84 ms, Memory: 13.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    stk = []
+    i=0
+    
+    while i < len(arr):
+        if stk == [] or stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] >= arr[i]:
+            stk.remove(stk[-1])
+    
+    return stk
