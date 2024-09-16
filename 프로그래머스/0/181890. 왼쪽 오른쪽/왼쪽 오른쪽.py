dad5bae2b64e7d448236807d2b364d0560f1e1fa
Sanga <103806889+tkddk0108@users.noreply.github.com>
2024-09-17 00:03:09 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,12 @@
+def solution(str_list):
+    answer = []
+    for i in range(len(str_list)):
+        if str_list[i] == "l":
+            for j in range(i):
+                answer.append(str_list[j])
+            return answer  
+        if str_list[i] == "r":
+            for j in range(i+1, len(str_list)):
+                answer.append(str_list[j])
+            return answer
+    return answer
