ee4a00e5938f7f7800f8cf4a510c237406d13b29
Sanga <103806889+tkddk0108@users.noreply.github.com>
2024-10-05 17:58:12 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,12 +1,2 @@
 def solution(money):
-    answer = []
-    for i in range(190):
-        if money < 5500:
-            answer = [0, money]
-            return answer
-        elif money - 5500*i == 0:
-            answer = [i, money - 5500*i]
-            return answer
-        elif money - 5500*i <= 0:
-            answer =  [i-1, money - 5500*(i-1)]
-            return answer
+    return (money//5500, money%5500)
