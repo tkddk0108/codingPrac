8151b6a1a61fe548337bb7b622f1274787730174
Sanga <103806889+tkddk0108@users.noreply.github.com>
2024-10-29 18:10:31 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,9 +1,7 @@
 def solution(n):
-    answer = 0
     val = [0]
     #tsn = ['3','6','9']
     for i in range(n*5):
         if '3' not in str(i) and i%3 != 0:
             val.append(i)
-    print(val)
     return val[n]
