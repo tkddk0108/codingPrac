2280a92ecf2996ea5e216f014b2e625b76878540
Sanga <103806889+tkddk0108@users.noreply.github.com>
2025-03-30 22:27:34 +0900

    [level 1] Title: 음양 더하기, Time: 0.21 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,6 +1,8 @@
 def solution(absolutes, signs):
     answer = 0
-    for i in range(len(absolutes)):
-        if signs[i] == True: answer += absolutes[i] 
-        else: answer -= absolutes[i] 
+    for i in range(len(signs)):
+        if signs[i] == True:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
     return answer
