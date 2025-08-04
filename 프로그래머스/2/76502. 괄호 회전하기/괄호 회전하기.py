614941c3ded9b0c55b944194cf14ccdee16c73fc
Sanga <103806889+tkddk0108@users.noreply.github.com>
2025-08-04 21:11:48 +0900

    [level 2] Title: 괄호 회전하기, Time: 62.25 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,20 +1,20 @@
 def check(s):
+    pair={"}":"{","]":"[",")":"("}
     stack = []
-    pair = {']':'[', '}':'{', ')':'('}
     for char in s:
-        if char in "{([":
+        if char in "{[(":
             stack.append(char)
-        elif char in "]})":
+        elif char in "}])":
             if not stack or stack[-1] != pair[char]:
                 return False
             stack.pop()
-            
     return len(stack) == 0
-    
+
 def solution(s):
     answer = 0
     for i in range(len(s)):
         if check(s):
             answer += 1
         s = s[1:]+s[0]
+            
     return answer
