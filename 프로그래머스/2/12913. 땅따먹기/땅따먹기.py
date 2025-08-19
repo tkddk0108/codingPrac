6860c844e2d3c9ba881cfa5f4c16eb4375ee17d2
Sanga <103806889+tkddk0108@users.noreply.github.com>
2025-08-19 16:50:52 +0900

    [level 2] Title: 땅따먹기, Time: 169.34 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,13 +1,9 @@
 def solution(land):
-    total = 0
-    a,b,c,d = land[0][0],land[0][1],land[0][2],land[0][3] 
-    for i in range(1,len(land)):
-        e,f,g,h = a,b,c,d
-        a,b,c,d = land[i][0],land[i][1],land[i][2],land[i][3] 
-        a += max(f,g,h)
-        b += max(e,g,h)
-        c += max(e,f,h)
-        d += max(e,f,g)
-
-
-    return max(a,b,c,d)
+    answer = 0
+    for i in range(1, len(land)):
+        land[i][0] += max(land[i-1][1],land[i-1][2],land[i-1][3])
+        land[i][1] += max(land[i-1][0],land[i-1][2],land[i-1][3])
+        land[i][2] += max(land[i-1][0],land[i-1][1],land[i-1][3])
+        land[i][3] += max(land[i-1][0],land[i-1][1],land[i-1][2])
+        
+    return max(land[-1])
